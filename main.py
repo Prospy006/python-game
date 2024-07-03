538097a848f57ecbb44e7665545c9381d4875376
x64 <74852993+Prospy006@users.noreply.github.com>
2024-07-03 20:47:15 +0530

    Game

---
+++ main.py
@@ -0,0 +1,12 @@
+CharacterName = ["N/A", "Ryan", "Harshid", "Steve", "Fawaaz", "Nithilan", "Prospy", "JK"]
+CharacterData = [
+                [0, 0, 100, 0], #N/A (No class)
+                [3, 3, 97, 15], #Ryan (Warrior - Assasin)
+                [3, 3, 97, 15], #Harshid (Warrior - Paladin)
+                [3, 3, 97, 15], #Steve (Warrior - N/A)
+                [2, 5, 99, 25], #Fawaaz (Tank - Air Bomber)
+                [4, 4, 95, 10], #Nithilan (Mage - Pyromancer)
+                [2, 5, 99, 25], #Prospy (Tank - Mech)
+                [4, 4, 95, 10]  #JK (Mage - N/A)
+                #Attack, Defence, Speed, Health
+                ]
