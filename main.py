54f94c04257c9f71c4054ad91f64f8c1dea1134a
Prospy006 <74852993+Prospy006@users.noreply.github.com>
2024-07-13 20:55:44 +0530

    1.0.0.3 BETA
    - Added a completely working savefile feature.
    - Added class selections.
    - Added boss data.
    - Added enemy data.
    - Patched bugs.

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 import time
+import math
+import random
 
 
 #VARIABLES
@@ -59,17 +61,30 @@ def slowText(text):
         print(char, end='', flush=True)
         time.sleep(sleepTime)
 
-#def singlePlayerSaveFile():
-#    global savefileInput
-#    savefileInput = input(print("Hello, user! Do you have a savefile you would like to load? (Y/N): "))
-#    if savefileInput == "Y":
-#        savefilePath = input(print("Type out the path in which you have your save-file: "))
-#        savefile = open(savefilePath, "rt")
-#        print(savefile)
-#    elif savefileInput == "N":
-#        savefileName = input(print("We will be creating one for you. Name your save: "))
-#        savefile = open(savefileName, "x")
-#        print(savefile)
+def classSelection():
+    global selectedClass
+    sleepTime = 0.03
+    slowText("Please note the weaknesses listed below: \n")
+    slowText("Warriors are stronger against Archers.\n")
+    slowText("Archers are stronger against Tanks.\n")
+    slowText("Tanks are stronger against Warriors.\n")
+    slowText("Mages are weaker against other classes, but also do more damage against other classes.\n")
+    selectedClass = input("Here are four classes you can select from: \n - warrior \n - archer \n - tank \n - mage\nWhich class would you like to choose: ")
+    if selectedClass == "warrior":
+        for classSelectionLoop in range(0, 9):
+            ClassData[0][classSelectionLoop] = ClassData[1][classSelectionLoop]
+    elif selectedClass == "archer":
+        for classSelectionLoop in range(0, 9):
+            ClassData[0][classSelectionLoop] = ClassData[2][classSelectionLoop]
+    elif selectedClass == "tank":
+        for classSelectionLoop in range(0, 9):
+            ClassData[0][classSelectionLoop] = ClassData[3][classSelectionLoop]
+    elif selectedClass == "mage":
+        for classSelectionLoop in range(0, 9):
+            ClassData[0][classSelectionLoop] = ClassData[4][classSelectionLoop]
+    else:
+        slowText("Typo? Select again.\n")
+        classSelection()
 
 def menu():
     global sleepTime
@@ -84,38 +99,71 @@ def menu():
     if playMode == "singleplayer":
         savefileInput = input("Great, loner! Do you have a savefile? (Y/N): ")
         if savefileInput == "Y":
-            savefilePath = input("Type out the path in which you have your save-file: ")
-            savefile = open(savefilePath, "rt")
-            print(savefile)
+            savefilePath = input("What is the name of your save: ")
+            with open(savefilePath, "rt") as savefile:
+                print(savefile.read())
         else:
             savefileName = input("We will be creating one for you. Name your save: ")
-            savefile = open(savefileName, "x")
-            print(savefile)
+            with open(savefileName, "xt") as savefile:
+                classSelection()
+                for j in range(0, 9):
+                    savefile.write(f"{ClassData[0][j]}\n")
     elif playMode == "multiplayer":
         slowText("On BETA phase. Please try again after the next update. Thank you!")
         menu()
     else:
-        slowText("Wrong input? We will take you to the menu. Please don't do it again.")
-#FUNCTIONS END
+        slowText("Wrong input? We will take you to the menu. Please don't do it again.\n")
+        menu()
 
+#ARRAYS
 
 ClassName = ["TEST","Warrior","Archer","Tank","Mage","Paladin","Arbalist","Mech","Pyromancer","Assasin","Sheriff","Air Bomber","Necromancer"]
 ClassData = [
-                [0, 0, 100, 0, 0, 0, 10, 0, 0], #SELECTION CLASS
-                [3, 3, 97, 1, 15, 0, 10, 0, 0], #Warrior
-                [4, 2, 95, 1, 10, 0, 10, 0, 0], #Archer
-                [3, 4, 99, 1, 25, 0, 10, 0, 0], #Tank
-                [4, 2, 97, 2, 10, 0, 10, 0, 0], #Mage
-                [4, 5, 95, 1, 20, 0, 10, 0, 0], #Paladin
-                [6, 3, 98, 1, 25, 0, 10, 0, 0], #Arbalist
-                [6, 5, 95, 1, 35, 0, 10, 0, 0], #Mech
-                [4, 4, 95, 2, 10, 0, 10, 0, 0], #Pyromancer
-                [4, 4, 95, 1, 10, 0, 10, 0, 0], #Assasin
-                [4, 4, 95, 1, 10, 0, 10, 0, 0], #Sheriff
-                [4, 4, 95, 1, 10, 0, 10, 0, 0], #Air bomber
-                [4, 4, 95, 2, 10, 0, 10, 0, 0]  #Necromancer
+                 [0, 0, 100, 0, 0, 0, 10, 0, 0], #SELECTION CLASS
+                #ORIGINALS
+                 [3, 3, 97, 1, 15, 0, 10, 0, 0], #Warrior
+                 [4, 2, 95, 1, 10, 0, 10, 0, 0], #Archer
+                 [3, 4, 99, 1, 25, 0, 10, 0, 0], #Tank
+                 [4, 2, 97, 2, 10, 0, 10, 0, 0], #Mage
+                #VARIANTS S1
+                 [6, 7, 94, 2, 30, 0, 10, 0, 0], #Paladin
+                 [8, 4, 96, 2, 25, 0, 10, 0, 0], #Arbalist
+                 [6, 5, 95, 2, 50, 0, 10, 0, 0], #Mech
+                 [9, 5, 94, 4, 20, 0, 10, 0, 0], #Pyromancer
+                #VARIANTS S2
+                 [5, 4, 92, 2, 25, 0, 10, 0, 0], #Assasin
+                 [7, 5, 94, 2, 25, 0, 10, 0, 0], #Sheriff
+                [10, 3, 97, 2, 40, 0, 10, 0, 0], #Air bomber
+                 [4, 3, 95, 4, 25, 0, 10, 0, 0]  #Necromancer
                 #Attack, Defence, Speed, Intelligence, Health, XP obtained, XP required, Level, sP obtained
                 ]
+
+EnemyName = ["TEST", "Ant", "Skeleton Warrior", "Skeleton Archer", "Miltank", "Horness", "Kraken", "Siren"]
+EnemyData = [
+                [0, 0, 100, 0, 0],   #TEST
+                [1, 0, 99, 1, 3],    #Ant
+                #Birth Guardian
+                [5, 6, 95, 2, 25],   #Skeleton Warrior
+                [7, 4, 94, 3, 20],   #Skeleton Archer
+                #Skeletor
+                [6, 8, 97, 4, 30],   #Miltank
+                [9, 4, 94, 5, 20],   #Horness
+                #Minotaur
+                [6, 9, 90, 6, 40],   #Siren
+                [12, 10, 93, 8, 35], #Kraken
+                #Poseidon
+                #Attack, Defence, Speed, XP Drop, Health
+]
+
+BossName = ["TEST", "Birth Guardian", "Skeletor", "Minotaur", "Poseidon"]
+BossData = [
+                [0, 0, 100, 0, 0],     #TEST
+                [3, 2, 98, 9, 10],     #Birth Guardian
+                [10, 5, 95, 20, 35],   #Skeletor
+                [15, 8, 92, 40, 50],   #Minotaur
+                [25, 15, 88, 55, 75],  #Poseidon
+                #Attack, Defence, Speed, XP Drop, Health
+]
 #TEST
 #x = int(input("Input a value to add to the XP: "))
 #ClassData[0][5] += x
@@ -123,7 +171,7 @@ ClassData = [
 sleepTime = 0.1
 slowText("Loading..............\n")
 slowText("Patching up resources.......\n")
-slowText("Clearing cache...........\n")
+slowText("Booting v1.0.1...........\n")
 menu()
 #TEST
 #i = 0
--- server.py
+++ server.py
@@ -5,7 +5,7 @@ hostName = "localhost"
 serverPort = 6969
 
 
-class MyServer(BaseHTTPRequestHandler):
+class BattleServer(BaseHTTPRequestHandler):
     def do_GET(self):
         self.send_response(200)
         self.send_header("Content-type", "text/html")
@@ -16,7 +16,7 @@ class MyServer(BaseHTTPRequestHandler):
         self.wfile.write(bytes("</body></html>", "utf-8"))
 
 if __name__ == "__main__":
-    webServer = HTTPServer((hostName, serverPort), MyServer)
+    webServer = HTTPServer((hostName, serverPort), BattleServer)
     print(f"Server started at http://{hostName}:{serverPort}")
     try:
         webServer.serve_forever()
